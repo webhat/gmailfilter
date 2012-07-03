2a33cde281be0bf21d6958e045d8c91a0b3df8df
Daniël W. Crompton <daniel.crompton@gmail.com>
2012-07-03 13:17:35 +0200

    Removed alerts

--- js/mailfilter_rules.js
+++ js/mailfilter_rules.js
@@ -49,14 +49,14 @@ $(document).ready(function () {
         $.each(elems, function (i, elem) {
             switch (elem.nodeName.toLowerCase()) {
                 case 'label':
-                    alert(elem.nodeName.toLowerCase());
+                    //alert(elem.nodeName.toLowerCase());
                     break;
                 case "shouldarchive":
                 case "shouldstar":
                 case "shouldneverspam":
                 case "shouldalwaysmarkasimportant":
                 case "shouldtrash":
-                    alert(elem.nodeName.toLowerCase());
+                    //alert(elem.nodeName.toLowerCase());
                     break;
                 default:
                     extension += elem.nodeName.toLowerCase() + ":(" + $(elem).html() + ")";
